f847d41142d8e6d234cdc3964dc1d8bb6d9948c4
DESKTOP-UUV8FP2\E.V.E. 08 <dariusz.majchrowicz@gmail.com>
2017-09-27 23:36:57 +0200

    changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,18 +20,18 @@ var globals = {
  }
 
  function readDOM() {
-   dom.head = query("#head");
-   dom.imageHead = query(".bgHead");
-   dom.bottomHead = query(".bottom-head");
-   dom.overlayHead = query(".overlayHead");
+   dom.head = query('#head');
+   dom.imageHead = query('.bgHead');
+   dom.bottomHead = query('.bottom-head');
+   dom.overlayHead = query('.overlayHead');
  }
 
 function addEvents() {
   paralax(event);
-  dom.bottomHead.addEventListener("mouseover", ()=>{
+  dom.bottomHead.addEventListener('mouseover', ()=>{
     TweenMax.to(dom.overlayHead, .3, {opacity : 1})
   })
-  dom.bottomHead.addEventListener("mouseout", ()=>{
+  dom.bottomHead.addEventListener('mouseout', ()=>{
     TweenMax.to(dom.overlayHead, .3, {opacity : 0})
   })
 
@@ -40,13 +40,14 @@ function addEvents() {
 
 
 function introAnimate() {
-  let fullName = query("#full-name");
-  TweenMax.from(fullName, .7, {y : -200 + "%"})
+  let fullName = query('#full-name').innerHTML;
+  TweenMax.to(dom.overlayHead, .3, {opacity : 0})
+  console.log(fullName[1])
 }
 
 function paralax(event) {
-  dom.head.addEventListener("mousemove", (e)=>{
+  dom.head.addEventListener('mousemove', (e)=>{
     let offsetX = 4*(e.pageX/globals.width);
-    TweenMax.set(dom.imageHead, {x : -offsetX + "%"})
+    TweenMax.set(dom.imageHead, {x : -offsetX + '%'})
   })
 }
